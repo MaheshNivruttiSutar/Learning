d01c3f35becd3d9eca1cc11170f44a714359cc65
MaheshNivruttiSutar <maheshsutar254@gmail.com>
2024-06-12 18:48:17 +0530

    changes

--- script.js
+++ script.js
@@ -2,10 +2,84 @@
 //1. Arithmetic Operators +,-,*,/
 let a = 5;
 let b = 2;
-console.log("a = ", a);
-console.log("b = ", b);
-console.log("a + b = ", a + b);
-console.log("a - b = ", a - b);
-console.log("a * b = ", a * b);
-console.log("a / b = ", a / b);
-console.log("a % b = ", a % b);
+console.log("initial value of a = ", a);
+console.log("initial value of b = ", b);
+// console.log("a + b = ", a + b);
+// console.log("a - b = ", a - b);
+// console.log("a * b = ", a * b);
+// console.log("a % b = ", a % b);
+
+// //Modulus: Modulus retruning reminder
+// console.log("a / b = ", a / b);
+
+// //Exponentiation
+// console.log("a ** b = ", a ** b);  //5 power 2
+
+
+//***** Unary operators  ****//
+//Unary operators: a)Increment b)Decrement
+//Increment
+//a++   a+1
+//a--  a-1
+
+// a = a + 1;  //a++
+// console.log("a ", a);
+
+// console.log("a++ = ", a++); //first original value print
+// console.log("a = ", a);  //6 will print
+
+// //++a
+// console.log("++a = ", ++a); //Value change at first time
+
+
+// //Decrement
+// a = a - 1;  //a--
+// console.log("a ", a);
+
+// console.log("a-- = ", a--); //first original value print
+// console.log("a = ", a);
+
+// //++a
+// console.log("--a = ", --a); //Value change at first time
+
+
+
+//**** Assignment Operator ****/
+//= += -= *= %= **=
+// a += 4;  //a = a + 4
+// console.log("a = ", a);
+
+// a -= 4;  //a = a - 4
+// console.log("a = ", a);
+
+// a *= 4;  //a = a * 4
+// console.log("a = ", a);
+
+// a %= 4;  //a = a % 4
+// console.log("a = ", a);
+
+// a **= 4;  //a = a ** 4
+// console.log("a = ", a);
+
+
+// **** Comparison Operator ****//
+/* Equal to ==  , Equal to & type  ====  , Not equal to !=  , Not equal to & type  !=== */
+// console.log("a == b ", a == b);
+
+// let c = 5; //Number
+// let d = "5" //String
+// console.log("c == d ", c == d);
+// console.log("c === d ", c === d);
+
+// console.log("c != d ", c != d);
+// console.log("c !=== d ", c !== d);
+
+// console.log("5 < 3 ", 5 < 3);
+// console.log("5 > 3 ", 5 > 3);
+// console.log("5 <= 3 ", 5 <= 3);
+// console.log("5 >= 3 ", 5 >= 3);
+
+
+
+
+// **** Logical Operator ****//

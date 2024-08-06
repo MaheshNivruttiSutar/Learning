0b2817fc3abe8eded0a1685e1417bbfe7f36a8d6
MaheshNivruttiSutar <maheshsutar254@gmail.com>
2024-08-06 23:18:47 +0530

    calculator

--- apnaCollege/Calculator/app.js
+++ apnaCollege/Calculator/app.js
@@ -8,8 +8,25 @@ function clearDisplay() {
     document.getElementById('display').value = '';
 }
 
+function deleteDigit() {
+    const displayElement = document.getElementById('display');
+    const currentValue = displayElement.value;
+
+    if (currentValue.length === 1) {
+        displayElement.value = '';
+    } else {
+        displayElement.value = currentValue.slice(0, -1);
+    }
+}
+
+
 function calculateResult() {
     const display = document.getElementById('display');
+
+    if (display.value.trim() === '') {
+        return; // Do nothing if the display is empty
+    }
+
     try {
         display.value = eval(display.value); // Use eval to evaluate the expression
     } catch (e) {
